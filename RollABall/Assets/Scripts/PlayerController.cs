8015620dc0ab3063d7c1e71ecdd8c6252a885733
astikaa1 <71190214+astikaa1@users.noreply.github.com>
2021-01-02 13:17:18 +0530

    Roll A Ball

---
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    public float speed = 0;
+    private Rigidbody rb;
+    private int count;
+    private float MovementX;
+    private float MovementY;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        MovementX = movementVector.x;
+        MovementY = movementVector.y;
+    }
+
+    void FixedUpdate(){
+        Vector3 movement = new Vector3(MovementX,0.00f,MovementY);
+        rb.AddForce(movement * speed);
+
+    }
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+    private void OnTriggerEnter(Collider other) //called when it touches a collider
+    {   if(other.gameObject.CompareTag("PickUp")){
+        other.gameObject.SetActive(false);
+        count++;
+        SetCountText();
+
+        }                    
+
+    }
+
+    void SetCountText(){
+        countText.text = "Count: " + count.ToString();
+        if(count>=12){
+            winTextObject.SetActive(true);
+        }
+    }
+}
